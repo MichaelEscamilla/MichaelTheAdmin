ae85487ba136d027a872c04a0d2035e7593e90ae
Michael Escamilla <MichaelEscamilla@protonmail.com>
2023-09-26 00:21:06 -0700

    Update Invoke-OOBEDeploy.ps1

--- Invoke-OOBEDeploy.ps1
+++ Invoke-OOBEDeploy.ps1
@@ -119,7 +119,7 @@ $action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPower
 $taskFolder = $ShedService.GetFolder("\")
 # https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
 #$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "SYSTEM", $NULL, 5)
-$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "$($env:COMPUTERNAME)\defaultuser0", $NULL, 5)
+$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "$env:COMPUTERNAME\defaultuser0", $NULL, 5)
 
 
 # Import 'OSD' Module
